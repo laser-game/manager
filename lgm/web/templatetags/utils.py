6126adf99fa9d3fa495a9971a81b23d37fb48bdf
wykys <vvykys@gmail.com>
2018-02-06 16:04:11 +0100

    added templatetags

---
+++ lgm/web/templatetags/utils.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def index(l, i):
+    return l[int(i)]
+
+@register.filter(name='range')
+def range_(i):
+    return range(int(i))
