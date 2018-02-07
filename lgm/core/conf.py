c7f621dd9784c9d06c11f505a331e6c882a38844
wykys <vvykys@gmail.com>
2018-02-07 18:06:33 +0100

    updated core conf

--- lgm/core/conf.py
+++ lgm/core/conf.py
@@ -23,26 +23,26 @@ MAX_FN = 3
 MAX_EVENTS = 16
 
 DEFAULT_TEAM_NAMES = (
-    "Red team",
-    "Orange team",
-    "Yellow team",
-    "Green team",
-    "Aqua team",
-    "Blue team",
-    "Purple team",
-    "Pink team"
+    'Red team',
+    'Orange team',
+    'Yellow team',
+    'Green team',
+    'Aqua team',
+    'Blue team',
+    'Purple team',
+    'Pink team'
 )
 
 COLOR = (
-    "#000000",  # black
-    "#FF0000",  # red
-    "#FFA500",  # orange
-    "#FFFF00",  # yellow
-    "#00FF00",  # green
-    "#00FFFF",  # aqua
-    "#0000FF",  # blue
-    "#FF00FF",  # purple
-    "#FFC0CB",  # pink
+    '#000000',  # black
+    '#FF0000',  # red
+    '#FFA500',  # orange
+    '#FFFF00',  # yellow
+    '#00FF00',  # green
+    '#00FFFF',  # aqua
+    '#0000FF',  # blue
+    '#FF00FF',  # purple
+    '#FFC0CB',  # pink
 )
 
 MIN_COLOR = 0
@@ -58,4 +58,4 @@ MAX_BATTERY = 100
 SCORE_KILL_OPPONENTS = 100
 SCORE_KILL_PLAYMATE = -50
 
-core_settings = Settings(__name__, "CORE")
+core_settings = Settings(__name__, 'CORE')
