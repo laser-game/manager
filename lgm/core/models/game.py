f40a9c06dc4519054e8f98e25110177cfd65e3a4
wykys <vvykys@gmail.com>
2018-02-09 22:23:37 +0100

    attempt to correct the dependency

--- lgm/core/models/game.py
+++ lgm/core/models/game.py
@@ -1,28 +1,34 @@
 from django.db import models
 from .base import BaseModel
 
-class TypeGame(BaseModel):
-    # TODO: JSONField for configuration
-    # TODO: see https://github.com/nnseva/django-jsoneditor
+
+class TypeAction(BaseModel):
     pass
+
+
+class Action(BaseModel):
+    type_action = models.ForeignKey(TypeAction, on_delete=models.PROTECT)
+
+
+class Player(BaseModel):
+    pass
+
+
+class Team(BaseModel):
+    pass
+
+
+class GamePlayer(BaseModel):
+    team = models.ForeignKey(Team, on_delete=models.PROTECT)
+    game_player = models.ForeignKey(GamePlayer, on_delete=models.PROTECT)
+
+
+class TypeGame(BaseModel):
+    pass
+
 
 class Game(BaseModel):
     type_game = models.ForeignKey(TypeGame, on_delete=models.PROTECT)
     team = models.ForeignKey(Team, on_delete=models.PROTECT)
     game_player = models.ForeignKey(GamePlayer, on_delete=models.PROTECT)
     action = models.ForeignKey(Action, on_delete=models.PROTECT)
-
-class TypeAction(BaseModel):
-    action = models.ForeignKey(Action, on_delete=models.PROTECT)
-
-class Action(BaseModel):
-    pass
-
-class GamePlayer(BaseModel):
-    team = models.ForeignKey(Team, on_delete=models.PROTECT)
-
-class Player(BaseModel):
-    game_player = models.ForeignKey(GamePlayer, on_delete=models.PROTECT)
-
-class Team(BaseModel):
-    pass
