77ff160acdb909d2a63ebb118cb18c9f0166480b
wykys <vvykys@gmail.com>
2018-02-09 09:55:56 +0100

    updated checkbox js

--- lgm/web/static/web/js/gui.js
+++ lgm/web/static/web/js/gui.js
@@ -1,20 +1,32 @@
-function SetCheckBox(id, checked) {
+function CheckBoxHTML(checkbox, checked) {
     if (checked) {
-        $(id).html('<img src="/static/web/img/checked.svg" class="checkbox">');
+        checkbox.html('<img src="/static/web/img/checked.svg" class="checkbox">');
+        checkbox.val(true);
     }
     else {
-        $(id).html('<img src="/static/web/img/unchecked.svg" class="checkbox">');
+        checkbox.html('<img src="/static/web/img/unchecked.svg" class="checkbox">');
+        checkbox.val(false);
     }
 }
 
 function RadioHTML(radio, checked) {
-    if (checked || checked == 'checked') {
+    if (checked) {
         radio.html('<img src="/static/web/img/radio_on.svg" class="radio">');
         radio.val(true);
     }
     else {
         radio.html('<img src="/static/web/img/radio_off.svg" class="radio">');
         radio.val(false);
+    }
+}
+
+function SetCheckBox(checkbox, checked) {
+    var checkbox_id = '#' + 'CheckBox-' + checkbox;
+    if (checked || checked == 'checked') {
+        CheckBoxHTML($(checkbox_id), true);
+    }
+    else {
+        CheckBoxHTML($(checkbox_id), false);
     }
 }
 
@@ -28,28 +40,43 @@ function SetRadio(radio_group, value) {
     }
 }
 
+function GetCheckBox(checkbox) {
+    var checkbox_id = '#' + 'CheckBox-' + checkbox;
+    var value = false;
+    if ($(checkbox_id).val() === 'true') {
+        value = true;
+    }
+    return value;
+}
+
 function GetRadio(radio_group) {
     var radio_class = '.' + 'Radio-' + radio_group;
     var value = false;
-
     $(radio_class).each(function() {
         if ($(this).val() === 'true') {
             value = $(this).attr('id').split('-');
             value = parseInt(value[value.length - 1]);
         }
     });
-
     return value;
 }
 
 $(document).ready(function () {
     $("[class^='Radio']").each(function() {
         RadioHTML($(this), false);
+    });
 
+    $("[id^='CheckBox']").each(function() {
+        CheckBoxHTML($(this), false);
     });
 
     $("[class^='Radio']").click(function () {
         $('.' + $(this).attr("class")).each(function(){RadioHTML($(this), false);});
         RadioHTML($(this), true);
+    });
+
+    $("[id^='CheckBox']").click(function () {
+        var checkbox = $(this).attr('id').split('CheckBox-')[1];
+        SetCheckBox(checkbox, !GetCheckBox(checkbox));
     });
 });
