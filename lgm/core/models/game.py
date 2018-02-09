674a012ccd2e2f55c011fa2f40d100235bb5e1b8
wykys <vvykys@gmail.com>
2018-02-09 21:21:39 +0100

    created Game in core models

--- lgm/core/models/game.py
+++ lgm/core/models/game.py
@@ -1,10 +1,10 @@
-# coding=utf-8
-
-
+from django.db import models
 from .base import BaseModel
-
 
 class TypeGame(BaseModel):
     # TODO: JSONField for configuration
     # TODO: see https://github.com/nnseva/django-jsoneditor
     pass
+
+class Game(BaseModel):
+    type_game = models.ForeignKey(TypeGame, on_delete=models.CASCADE)
