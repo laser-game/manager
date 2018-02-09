46d09e8db7d4fd7f6b5b9ba323e5f39973a3d960
wykys <vvykys@gmail.com>
2018-02-10 00:47:29 +0100

    updates models

--- lgm/core/models/game.py
+++ lgm/core/models/game.py
@@ -2,14 +2,6 @@ from django.db import models
 from .base import BaseModel
 
 
-class TypeAction(BaseModel):
-    pass
-
-
-class Action(BaseModel):
-    type_action = models.ForeignKey(TypeAction, on_delete=models.PROTECT)
-
-
 class Player(BaseModel):
     pass
 
@@ -18,17 +10,24 @@ class Team(BaseModel):
     pass
 
 
-class GamePlayer(BaseModel):
-    team = models.ForeignKey(Team, on_delete=models.PROTECT)
-    game_player = models.ForeignKey(GamePlayer, on_delete=models.PROTECT)
-
-
 class TypeGame(BaseModel):
     pass
 
 
 class Game(BaseModel):
     type_game = models.ForeignKey(TypeGame, on_delete=models.PROTECT)
+
+
+class GamePlayer(BaseModel):
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
     team = models.ForeignKey(Team, on_delete=models.PROTECT)
-    game_player = models.ForeignKey(GamePlayer, on_delete=models.PROTECT)
-    action = models.ForeignKey(Action, on_delete=models.PROTECT)
+    player = models.ForeignKey(Player, on_delete=models.PROTECT)
+
+
+class TypeAction(BaseModel):
+    pass
+
+
+class Action(BaseModel):
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
+    type_action = models.ForeignKey(TypeAction, on_delete==models.PROTECT)
