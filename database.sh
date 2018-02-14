62d23b54f25edb956646a0535164fbfe059d3e96
wykys <vvykys@gmail.com>
2018-02-14 20:27:48 +0100

    fixed database script

--- database.sh
+++ database.sh
@@ -9,6 +9,7 @@ cd lgm
 rm db.sqlite3_old -f
 mv db.sqlite3 db.sqlite3_old
 mv core/migrations/0002_initial_data.py ./
+rm core/migrations/0001_initial.py -f
 ./manage.py migrate
 ./manage.py makemigrations
 mv 0002_initial_data.py core/migrations
