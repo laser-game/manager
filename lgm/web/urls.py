0f4cf33438386e7b42688bf970585601a662c6e3
Josef Kolář <thejoeejoee@gmail.com>
2018-02-14 13:46:09 +0100

    Add login page with form

--- lgm/web/urls.py
+++ lgm/web/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from .apps import WebConfig
@@ -6,6 +7,9 @@ from .views import index
 app_name = WebConfig.name
 urlpatterns = [
     # first has higher priority
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+
     path('settings', index.settings, name='settings'),
     path('stream', index.stream, name='stream'),
     path('archive', index.archive, name='archive'),
--- lgm/web/views/index.py
+++ lgm/web/views/index.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, reverse
 
 from core.conf import core_settings
 
 
+@login_required
 def settings(request):
     context = {
         'MAX_PLAYERS': core_settings.MAX_PLAYERS,
@@ -13,6 +14,7 @@ def settings(request):
     return render(request, 'web/settings/index.html', context)
 
 
+@login_required
 def stream(request):
     context = {
         'MAX_PLAYERS': core_settings.MAX_PLAYERS,
@@ -22,6 +24,7 @@ def stream(request):
     return render(request, 'web/stream/index.html', context)
 
 
+@login_required
 def archive(request):
     context = {
         'MAX_PLAYERS': core_settings.MAX_PLAYERS,
@@ -30,5 +33,7 @@ def archive(request):
     }
     return render(request, 'web/archive/index.html', context)
 
+
+@login_required
 def index(request):
     return redirect(reverse('web:settings'), permanent=True)
