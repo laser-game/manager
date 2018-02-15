4ff424b0b333789106c3c2548f153e5f5a58e2b6
wykys <vvykys@gmail.com>
2018-02-15 02:06:54 +0100

    importanize code refactored

--- lgm/core/models/__init__.py
+++ lgm/core/models/__init__.py
@@ -2,9 +2,10 @@ from .color import TypeColor
 from .event import Event, TypeEvent
 from .game import Game, TypeGame
 from .player import GamePlayer, Player
-from .switch import TypeSwitch, TypeGameSwitch, Switch
+from .switch import Switch, TypeGameSwitch, TypeSwitch
 from .team import Team
 from .vest import Vest
+
 
 __all__ = [
     'TypeColor',
