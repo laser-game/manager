ce4919915a19eef3fa49d7b474841790222e0c1d
wykys <vvykys@gmail.com>
2018-06-21 02:32:29 +0200

    added first test load data to database

--- lgm/api/views/game.py
+++ lgm/api/views/game.py
@@ -1,20 +1,12 @@
 from django.http import HttpResponse
 import json
 
+from core import game_logic
+
 
 def game_config(request):
     if request.method == "POST" and request.is_ajax():
-        config = json.loads(request.body)
-
-        for i in config:
-            print('\n======================\n', i, '\n======================')
-            for j in config[i]:
-                try:
-                    print(j, config[i][j])
-                except:
-                    print(j)
-
-        print('OK')
+        game_logic.set_game(json.loads(request.body))
         return HttpResponse('ok')
     else:
         return HttpResponse('error')
---
+++ lgm/core/game_logic/logic.py
@@ -0,0 +1,52 @@
+from ..models import TypeColor, Event, TypeEvent, Game, TypeGame, GamePlayer, Player, Switch, TypeGameSwitch, TypeSwitch, Team, Vest
+
+from datetime import datetime
+
+
+def set_game(config):
+    set_game = config['SetGame']
+    players = config['Player']
+    teams = config['Team']
+
+    for i in config:
+        print('\n======================\n', i, '\n======================')
+        for j in config[i]:
+            if type(j) is str:
+                print(j, config[i][j])
+            else:
+                print(j)
+
+    print(set_game)
+    print(players)
+    print(teams)
+
+    dat_game = Game()
+    dat_game.game_type_game = TypeGame.objects.all()[0]
+    dat_game.type_game_id = TypeGame.objects.all()[0].id
+    dat_game.state = 'S'
+    dat_game.start = datetime.today()
+    dat_game.save()
+
+
+
+    for player in players:
+        if player['enable']:
+            print(player)
+            dat_player = Player()
+            dat_player.name = player['name']
+            dat_player.save()
+
+            color = TypeColor.objects.all().filter(index=player['color_index'])[0]
+            vest = Vest.objects.all().filter(address=player['address'])[0]
+
+            dat_game_player = GamePlayer()
+            dat_game_player.player = dat_player
+            dat_game_player.game = dat_game
+            dat_game_player.type_color = color
+            dat_game_player.vest = vest
+            dat_game_player.points = 0
+            dat_game_player.shots_count = 0
+            dat_game_player.kills_count = 0
+            dat_game_player.deaths_count = 0
+            dat_game_player.friendly_kills_count = 0
+            dat_game_player.save()
