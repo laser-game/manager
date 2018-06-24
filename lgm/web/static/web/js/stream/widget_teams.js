ef83855d81c411dbb416832d20b50b1581ef4aac
wykys <vvykys@gmail.com>
2018-06-24 19:48:23 +0200

    updated actual team api

--- lgm/web/static/web/js/stream/widget_teams.js
+++ lgm/web/static/web/js/stream/widget_teams.js
@@ -1,28 +1,6 @@
 // načteme informace týmech
 function GetActualTeam() {
-    $.getJSON("/api/actual-teams", function (Team) {
-        var order = [];
-        var position;
-        for (j = 0; j < Team.length; j++) {
-            position = Team.length + 1;
-            for (i = 0; i < Team.length; i++) {
-                if (Team[i].Position < position && Team[i].enable) {
-                    var order_test = true;
-                    if (order.length) {
-                        for (k = 0; k < order.length; k++) {
-                            if (order[k] == Team[i].id) {
-                                order_test = false;
-                                break;
-                            }
-                        }
-                    }
-                    if (order_test) {
-                        position = Team[i].Position;
-                        order[j] = Team[i].id;
-                    }
-                }
-            }
-        }
+    $.getJSON("/api/actual-teams", function (Teams) {
 
         HTML_code = '<tr>';
         HTML_code += '<th> POŘADÍ </th>';
@@ -32,28 +10,28 @@ function GetActualTeam() {
         HTML_code += '<th> SKÓRE </th>';
         HTML_code += '</tr>';
 
-        for (j = 0; j < order.length; j++) {
-            i = order[j];
+        for (i in Teams) {
+            Team = Teams[i]
             HTML_code += '<tr>';
 
             HTML_code += '<td align="center">';
-            HTML_code += Team[i].Position.toString();
+            HTML_code += Team.position
             HTML_code += '</td>';
 
             HTML_code += '<td align="center">';
-            HTML_code += '<span style="color: ' + Color[Team[i].color_index].rgb + '; font-weight: 400;">' + Team[i].name + '</span>' + '<br>';
+            HTML_code += '<span style="color: ' + Team.color + '; font-weight: 400;">' + Team.name + '</span>' + '<br>';
             HTML_code += '</td>';
 
             HTML_code += '<td align="center">';
-            HTML_code += Team[i].Kill.toString();
+            HTML_code += Team.kills_count
             HTML_code += '</td>';
 
             HTML_code += '<td align="center">';
-            HTML_code += Team[i].Death.toString();
+            HTML_code += Team.deaths_count
             HTML_code += '</td>';
 
             HTML_code += '<td align="center">';
-            HTML_code += Team[i].Score.toString();
+            HTML_code += Team.points
             HTML_code += '</td>';
 
             HTML_code += '</tr>';
